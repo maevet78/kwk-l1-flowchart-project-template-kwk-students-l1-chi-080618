2ffeb795f53eae81b39dacfe6ce94784e194dc26
maevet78 <190439mt@saintviator.com>
2018-08-16 14:42:50 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,12 +29,12 @@ class ApplicationController < Sinatra::Base
       img="https://img.makeupalley.com/hwthumb/980/980/1_1_8_1_3210237.JPG"
       url="https://www.sephora.com/product/teint-idole-ultra-24h-long-wear-foundation-P308201"
     
-    elsif @user_ans == 23110
-      dogName="Cavalier King Charles"
-      welcome ="Thank you for taking our quiz, #{@user_name}. <br> <b> #{dogName}</a> </b> is the dog we've decided will works best for you!"
-      descrip="They can adapt very well to apartment living, are very good for new owners, but can not tolerate being alone very well. They can tolerate the cold well but the not the best and the same with the heat. They are very friendly towards everyone, including other dogs. They shed a little bit but not much, don’t drool, are relatively easy to groom, have potential to gain weight, and are not large dogs. They are pretty easy to train, have a relatively high energy level, need plenty of exercise, and are extremely playful." 
-      img="https://www.omlet.us/images/cache/1024/682/Dog-Cavalier_King_Charles_Spaniel-Three_little_Cavalier_King_Charles_Spaniel's_sitting_patiently.jpg"
-      url="https://en.wikipedia.org/wiki/Cavalier_King_Charles_Spaniel"
+    elsif answer == "AEI"
+      foundName="DIOR Forever Perfect Foundation"
+      welcome ="Thank you for taking our quiz. <br> <b> #{foundName}</a> </b> is the foundation that will work best for you!<br>"
+      descrip="#{foundName} is priced at $52 at Sephora"
+      img="https://media1.popsugar-assets.com/files/thumbor/6xUGvKWh3Zz7hFAWhtWqmz6T0LY/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2018/02/26/720/n/43659231/865f901f03ff7d8d_diorskinmaturefoundatin/i/Dior-Diorskin-Forever-Perfect-Foundation.jpg"
+      url="https://www.sephora.com/product/diorskin-forever-perfect-foundation-broad-spectrum-35-P405059"
   end
 
 post '/results2' do
